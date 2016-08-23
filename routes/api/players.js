67e34dee5b6e186d97a57db115dffaa1695ef86b
Corey Pauley <cpauleyrun@gmail.com>
2016-08-23 05:23:37 -0500

    Ignore: Made players routes more robust and organized

--- routes/api/players.js
+++ routes/api/players.js
@@ -3,7 +3,16 @@
 var players = require('libs/data/players');
 
 module.exports = function(app, apiRouter) {
-    apiRouter.post('/players', function(request, response) {
+    apiRouter.get('/players/?', function(request, response) {
+
+        players.getAllPlayers()
+            .then((players) => {
+                response.status(200).json(players);
+            })
+            .catch((err) => response.status(500).json({'error': err}));
+    });
+
+    apiRouter.post('/players/?', function(request, response) {
 
         const player = request.body;
         //console.log(player);
@@ -15,16 +24,7 @@ module.exports = function(app, apiRouter) {
             .catch((err) => response.status(500).json({'error': err}));
     });
 
-    apiRouter.get('/players', function(request, response) {
-
-        players.getAllPlayers()
-            .then((players) => {
-                response.status(200).json(players);
-            })
-            .catch((err) => response.status(500).json({'error': err}));
-    });
-
-    apiRouter.get('/players/:id', function(request, response) {
+    apiRouter.get('/players/:id/?', function(request, response) {
         players.findPlayer(request.params.id)
             .then((player) => {
                 if (player === null) {
@@ -37,7 +37,7 @@ module.exports = function(app, apiRouter) {
             .catch((err) => response.status(500).json({'error': err}));
     });
 
-    apiRouter.delete('/players/:id', function(request, response) {
+    apiRouter.delete('/players/:id/?', function(request, response) {
         players.deletePlayer(request.params.id)
             .then(() => response.status(204).json({'message': `Successfully deleted player with id: ${request.params.id}`}))
             .catch((err) => response.status(500).json({'error': err}));
