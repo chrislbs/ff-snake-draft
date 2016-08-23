0f799072c60f3adf77c4880237f5316802c84d43
Chris Pounds <chrislbs@gmail.com>
2016-08-22 19:32:07 -0500

    Adding dummy rest APIs to send/receive data

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const data = require('./libs/data');
 const app = express();
 const port = 8080;
+
+app.use((request, response, next) => {
+    //console.log(request.headers);
+    next()
+});
+
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
     response.send('Hello from Express!');
 });
 
-app.use((request, response, next) => {
-    console.log(request.headers);
-    next()
+app.get('/users', function(request, response) {
+
+    data.getUsers()
+        .then(function(users) {
+            response.send(users);
+        })
+        .catch(function(err) {
+            console.log(err)
+            response.status(500).send(err);
+        });
+});
+
+app.post('/users', function(request, response) {
+
+    const user = request.body;
+    console.log(user);
+    data.createUser(user)
+        .then(function() {
+            console.log(arguments)
+            response.send(`${user.username} created`);
+        })
+        .catch(function(err) {
+            console.log(err)
+            response.status(500).send(err);
+        });
 });
 
 app.use((err, request, response, next) => {
