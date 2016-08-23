ffad52e77d7aeee8b03f0fadf5dfdeb8a808fc34
Corey Pauley <cpauleyrun@gmail.com>
2016-08-23 08:23:40 -0500

    Making user actions similar to player actions

--- assets/js/actions/userActions.jsx
+++ assets/js/actions/userActions.jsx
@@ -32,3 +32,33 @@ export function createUser(username, age) {
         }
     };
 };
+
+export const GET_USER = 'GET_USER';
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
+export const GET_USER_FAILURE = 'GET_USER_FAILURE';
+
+export function getUser(id) {
+    return {
+        [CALL_API]: {
+            endpoint: `/users/${id}/?`,
+            types: [GET_USER, GET_USER_SUCCESS, GET_USER_FAILURE]
+        }
+    };
+};
+
+export const DELETE_USER = 'DELETE_USER';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAILURE = 'DELETE_USER_FAILURE';
+
+export function deleteUser(id) {
+    return {
+        [CALL_API]: {
+            endpoint: `/users/${id}/`,
+            types: [DELETE_USER, DELETE_USER_SUCCESS, DELETE_USER_FAILURE],
+            method: 'DELETE',
+            data: {
+                id
+            }
+        }
+    };
+};
