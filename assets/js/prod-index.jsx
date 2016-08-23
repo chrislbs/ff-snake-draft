ffa50452189dc6db3413c9f2cda173dc44c4ddd6
Corey Pauley <cpauleyrun@gmail.com>
2016-08-23 00:15:57 -0500

    Added entrypoint for client-side JS code that sets up ReactJS with React-Router SPA routing and a Redux wrapper for global state management

---
+++ assets/js/prod-index.jsx
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import { Router, browserHistory, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import routes from './routes';
+
+const store = configureStore('prod');
+
+const historyType = window.IEVersion && window.IEVersion < 10 ? hashHistory : browserHistory;
+
+const history = syncHistoryWithStore(historyType, store, {selectLocationState: state => state.router});
+
+render(
+    <Provider store={store}>
+        <Router history={history}>
+            {routes}
+        </Router>
+    </Provider>,
+    document.getElementById('react-root')
+);
