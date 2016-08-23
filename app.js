88ce84135b2f15d17b1713fb1f8c3c097a98cf13
Chris Pounds <chrislbs@gmail.com>
2016-08-22 21:15:34 -0500

    Adding a players table and accessor methods
    - Including moment for date parsing
    
    - Creating basic CRUD data operations as well as REST apis

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const data = require('./libs/data');
+const players = require('./libs/data/players');
 const app = express();
 const port = 8080;
 
@@ -40,6 +41,46 @@ app.post('/users', function(request, response) {
             console.log(err)
             response.status(500).send(err);
         });
+});
+
+app.post('/players', function(request, response) {
+
+    const player = request.body;
+    console.log(player);
+    players.createPlayer(player)
+        .then((playerId) => {
+            player.id = playerId;
+            return player;
+        })
+        .catch((err) => response.status(500).send(err));
+});
+
+app.get('/players', function(request, response) {
+
+    players.getAllPlayers()
+        .then((players) => {
+            response.send(players);
+        })
+        .catch((err) => response.status(500).send(err));
+});
+
+app.get('/players/:id', function(request, response) {
+    players.findPlayer(request.params.id)
+        .then((player) => {
+            if (player == null) {
+                response.status(404).send();
+            }
+            else {
+                response.send(player);
+            }
+        })
+        .catch((err) => response.status(500).send(err))
+});
+
+app.delete('/players/:id', function(request, response) {
+    players.deletePlayer(request.params.id)
+        .then(() => response.status(204).send())
+        .catch((err) => response.status(500).send(err))
 });
 
 app.use((err, request, response, next) => {
---
+++ libs/data/players.js
@@ -0,0 +1,92 @@
+const mysql = require('promise-mysql');
+const moment = require('moment');
+const Promise = require('bluebird');
+const db = require('./db');
+
+/**
+ * Create the table for storing players if it doesn't exist using the provied connection.
+ *
+ * @param connection The db connection to execute the create table statement against
+ * @returns {Promise} A promise that returns the provided connection
+ */
+function createPlayerTable(connection) {
+    var stmt = `
+    CREATE TABLE IF NOT EXISTS players (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(50) NOT NULL,
+        birthday DATE NOT NULL,
+        UNIQUE KEY (name, birthday),
+        PRIMARY KEY (id))`;
+    return connection.query(stmt).then( () => connection);
+}
+
+/**
+ * Create a player in the table. The player object should be in the form:
+ * {
+ *   name: '<first> <last>',
+ *   birthday: 'yyyyMMdd'
+ * }
+ *
+ * @param player The player to persist into the players table
+ * @returns {Promise} A promise that returns the id of the row for the player
+ */
+function createPlayer(player) {
+    var data = {
+        name : player.name,
+        birthday : moment(player.birthday).format('YYYY-MM-DD')
+    };
+    console.log(data);
+    return Promise.using(db.getConnection(), createPlayerTable)
+        .then((connection) => {
+            return connection.query('INSERT INTO players SET ?', data)
+        })
+        .then((result) => result.insertId);
+}
+
+/**
+ * Retrieve all rows from the player table
+ *
+ * @returns {Promise} A promise that returns all of the rows in the table
+ */
+function getAllPlayers() {
+    return Promise.using(db.getConnection(), createPlayerTable)
+        .then((connection) => {
+            return connection.query('SELECT * FROM players')
+        });
+}
+
+/**
+ * Retrieve the player from the table with the specified id
+ *
+ * @param playerId The id of the player to retrieve
+ * @returns {Promise} A promise that returns the player or null if one wasn't found
+ */
+function findPlayer(playerId) {
+    return Promise.using(db.getConnection(), createPlayerTable)
+        .then((conn) => {
+            var stmt = mysql.format('SELECT * FROM players WHERE id = ?', [playerId]);
+            return conn.query(stmt)
+        })
+        .then((rows) => {
+            return rows[0]
+        });
+}
+
+/**
+ * Delete the player from the table with the specified id
+ *
+ * @param playerId The id of the player to delete
+ * @returns {Promise} A promise that returns the result of the delete query
+ */
+function deletePlayer(playerId) {
+    return Promise.using(db.getConnection(), createPlayerTable)
+        .then((conn) => {
+            var del = mysql.format('DELETE FROM players where id = ?', [playerId]);
+            return conn.query(del);
+        });
+}
+
+module.exports.createPlayer = createPlayer;
+module.exports.getAllPlayers = getAllPlayers;
+module.exports.findPlayer = findPlayer;
+module.exports.deletePlayer = deletePlayer;
