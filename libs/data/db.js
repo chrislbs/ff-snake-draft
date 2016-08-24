18d0beb3a5fb0400343536f34f5deccc22e72de4
Corey Pauley <cpauleyrun@gmail.com>
2016-08-24 10:50:00 -0500

    Try to get connection details for MySQL docker container, falling back to the original connection settings

--- libs/data/db.js
+++ libs/data/db.js
@@ -1,11 +1,13 @@
+'use strict';
+
 const mysql = require('promise-mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: process.env.MYSQL_PORT_3306_TCP_ADDR || 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'ff'
+    password: process.env.MYSQL_ENV_MYSQL_ROOT_PASSWORD || 'root',
+    database: process.env.MYSQL_ENV_MYSQL_DATABASE || 'ff'
 });
 
 /**
@@ -14,11 +16,11 @@ const pool = mysql.createPool({
  */
 function getConnection() {
     return pool.getConnection().disposer(function(connection) {
-        pool.releaseConnection(connection)
+        pool.releaseConnection(connection);
     });
 }
 
-process.on('SIGTERM', function() { pool.end() });
-process.on('SIGINT', function() { pool.end() });
+process.on('SIGTERM', function() { pool.end(); });
+process.on('SIGINT', function() { pool.end(); });
 
 module.exports.getConnection = getConnection;
