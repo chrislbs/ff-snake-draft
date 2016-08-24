8793dc07b8756913afca722e98e0a0709c81bb88
Corey Pauley <cpauleyrun@gmail.com>
2016-08-24 17:07:13 -0500

    Removed user endpoints and changed the public folder entire node server isn't accessible

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
-    data = require('./libs/data'),
     utils = require('./libs/utils'),
     apiRouter = express.Router(),
     _ = require('lodash'),
@@ -31,35 +30,8 @@ _.each(utils.getModulesInDirectory(app.get('paths').routes, ['_', '.']), functio
 
 app.use('/api', apiRouter);
 
-app.get('/users', function(request, response) {
-
-    data.getUsers()
-        .then(function(users) {
-            response.send(users);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
-});
-
-app.post('/users', function(request, response) {
-
-    const user = request.body;
-    console.log(user);
-    data.createUser(user)
-        .then(function() {
-            console.log(arguments);
-            response.send(`${user.username} created`);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
-});
-
 // Needs to come after all of the other middleware so you don't expose your server code
-app.use('/', express.static('public'));
+app.use('/public', express.static('public'));
 
 app.listen(config.server.port, (err) => {
     if (err) {
