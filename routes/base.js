6ab915e1dd02333d50b8856dfae483783680fa36
Corey Pauley <cpauleyrun@gmail.com>
2016-08-24 13:18:24 -0500

    Just a basic ping route that gives some stats on the node server

---
+++ routes/base.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var os = require('os'),
+    v8 = require('v8'),
+    throttle = require('middleware/_throttle');
+
+module.exports = function(app) {
+    // This request needs to be throttled to 1 request every 10 seconds
+    app.all('/ping/?', throttle({interval: 10000, holdTime: 5000}), function (req, res) {
+        res.status(200).json({
+            'uptime': new Date().getTime() - app.start_time,
+            'cpu': os.cpus(),
+            'memory': {
+                'total': os.totalmem(),
+                'free': os.freemem(),
+            },
+            'load_avg': os.loadavg(),
+            'heap': v8.getHeapStatistics()
+        });
+    });
+};
