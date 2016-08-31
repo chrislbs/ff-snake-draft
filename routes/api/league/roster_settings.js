46bde2612ef5431814bc79f69c6da99ee7af86c2
Chris Pounds <chrislbs@gmail.com>
2016-08-30 22:51:20 -0500

    Hacked together some validations

--- routes/api/league/roster_settings.js
+++ routes/api/league/roster_settings.js
@@ -4,6 +4,28 @@ const express = require('express'),
     _ = require('lodash'),
     roster = require('../../../libs/data/roster');
 
+const positionOrder = {
+    "QB" : 0,
+    "WR" : 1,
+    "RB" : 2,
+    "TE" : 3,
+    "FLEX" : 4,
+    "K" : 5,
+    "DST" : 6,
+    "D" : 7,
+    "DB" : 8
+};
+
+function orderPositions(positions) {
+    positions.sort((lhs, rhs) => {
+        return positionOrder[lhs] > positionOrder[rhs];
+    });
+    return positions;
+}
+
+function validPositions(positions) {
+    return _.every(positions, (pos) => positionOrder[pos] != null);
+}
 
 /**
  * Update the league's roster settings
@@ -12,11 +34,18 @@ router.put('/', function(request, response) {
 
     const positions = request.body;
     const leagueId = request.leagueId;
-    roster.setRosterSettings(leagueId, positions)
-        .then(() => {
-            response.send(positions);
-        })
-        .catch((err) => response.status(500).send(err));
+
+    if (validPositions(positions)) {
+        roster.setRosterSettings(leagueId, positions)
+            .then(() => {
+                response.send(orderPositions(positions));
+            })
+            .catch((err) => response.status(500).send(err));
+    }
+    else {
+        response.status(400).send({ err : "invalidPositions" })
+    }
+
 });
 
 /**
@@ -28,7 +57,7 @@ router.get('/', function(request, response) {
     roster.getRosterSettings(leagueId)
         .then((rows) => {
             var positions = _.map(rows, (row) => row.position );
-            response.send(positions);
+            response.send(orderPositions(positions));
         })
         .catch((err) => response.status(500).send(err));
 });
